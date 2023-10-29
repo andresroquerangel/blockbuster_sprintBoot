b2a3a45bf1de14d62a1e89bcf7dd47293e718133
andresroquerangel <20030928@itcelaya.edu.mx>
2023-10-28 18:13:41 -0600

    Spring Security

--- src/main/java/com/example/modeladov1/repository/UsuarioRepository.java
+++ src/main/java/com/example/modeladov1/repository/UsuarioRepository.java
@@ -4,7 +4,11 @@ import com.example.modeladov1.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
     Usuario findById(int id);
+
+    Optional<Usuario> findOneByEmail(String email);
 }
---
+++ src/main/java/com/example/modeladov1/security/JWTAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.example.modeladov1.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.Authorization;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        AuthCredentials authCredentials = new AuthCredentials();
+        try{
+            authCredentials = new ObjectMapper().readValue(request.getReader(), AuthCredentials.class);
+        }catch(IOException e){
+        }
+
+        UsernamePasswordAuthenticationToken userNamePAT = new UsernamePasswordAuthenticationToken(authCredentials.getEmail(),authCredentials.getPassword(), Collections.emptyList());
+        return getAuthenticationManager().authenticate(userNamePAT);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authResult.getPrincipal();
+
+        String token = TokenUtil.createToken(userDetails.getNombre(),userDetails.getUsername());
+
+        response.addHeader("Authorization","Bearer "+token);
+        response.getWriter().flush();
+
+        super.successfulAuthentication(request,response,chain,authResult);
+    }
+}
