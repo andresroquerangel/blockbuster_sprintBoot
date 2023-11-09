0f65e503503cf13ad8dbe674b16d19434386556a
almagpm <20030274@itcelaya.edu.mx>
2023-11-09 14:26:54 -0600

    Correcion de la estructura del modelo, servicio, repositorio y controlador de Producto, TipoProducto

---
+++ src/test/java/com/example/modeladov1/service/VentaDetalleServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.modeladov1.service;
+
+import com.example.modeladov1.repository.VentaDetalleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import com.example.modeladov1.model.VentaDetalle;
+import com.example.modeladov1.model.Venta;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class VentaDetalleServiceTest {
+    @Mock
+    private VentaDetalleRepository repo;
+
+    @InjectMocks
+    private VentaDetalleService ventaDetalleService;
+
+    private VentaDetalle ventaDetalle;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        ventaDetalle = new VentaDetalle();
+        ventaDetalle.setVenta(new Venta());
+        ventaDetalle.setId_venta_detalle(1);
+    }
+
+    @Test
+    void getAll() {
+        when(repo.findAll()).thenReturn(Arrays.asList(ventaDetalle));
+        assertNotNull(ventaDetalleService.getAll());
+    }
+}
